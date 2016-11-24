6a55569edf4fc53678ca2b623dd2b9ade30e5b6a
Sebazzz <triadsebas@gmail.com>
2016-11-24 21:12:39 +0100

    Use `placeholder-for` attribute (via `PlaceHolderForTagHelper`) to autogen placeholder

---
+++ src/IFS.Web/Framework/Views/TagHelpers/PlaceHolderForTagHelper.cs
@@ -0,0 +1,23 @@
+﻿// ******************************************************************************
+//  © 2016 Sebastiaan Dammann - damsteen.nl
+// 
+//  File:           : PlaceHolderForTagHelper.cs
+//  Project         : IFS.Web
+// ******************************************************************************
+
+namespace IFS.Web.Framework.Views.TagHelpers {
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("input", Attributes = PlaceHolderForAttribute)]
+    public sealed class PlaceHolderForTagHelper : TagHelper {
+        private const string PlaceHolderForAttribute = "placeholder-for";
+
+        [HtmlAttributeName(PlaceHolderForAttribute)]
+        public ModelExpression Target { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output) {
+            output.Attributes.SetAttribute("placeholder", this.Target.Metadata.DisplayName ?? this.Target.Metadata.PropertyName);
+        }
+    }
+}
