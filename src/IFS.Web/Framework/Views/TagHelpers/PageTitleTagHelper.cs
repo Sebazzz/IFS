5721ea13ef3f69f9d48c2ba86e4f90878f11583c
Sebazzz <triadsebas@gmail.com>
2016-11-24 22:25:46 +0100

    Implement "title-subtitle" using common tag helper

---
+++ src/IFS.Web/Framework/Views/TagHelpers/PageTitleTagHelper.cs
@@ -0,0 +1,40 @@
+﻿// ******************************************************************************
+//  © 2016 Sebastiaan Dammann - damsteen.nl
+// 
+//  File:           : PageTitleTagHelper.cs
+//  Project         : IFS.Web
+// ******************************************************************************
+
+namespace IFS.Web.Framework.Views.TagHelpers {
+    using System.Diagnostics.CodeAnalysis;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Html;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    /// <summary>
+    /// Shows a default title
+    /// </summary>
+    [HtmlTargetElement("ifs-page-title")]
+    public sealed class PageTitleTagHelper : TagHelper {
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        [SuppressMessage("ReSharper", "MustUseReturnValue", Justification = "IHtmlBuilder returns itself ('this' instance)")]
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output) {
+            TagHelperContent tagContents = await output.GetChildContentAsync();
+            string tagContentsString = tagContents.GetContent(NullHtmlEncoder.Default);
+
+            TagBuilder innerBuilder = new TagBuilder("small");
+            innerBuilder.InnerHtml.Append(tagContentsString);
+
+            output.TagName = "h1";
+            output.Content.Clear()
+                          .AppendLine("Internet File System")
+                          .AppendHtml(innerBuilder);
+        }
+    }
+}
