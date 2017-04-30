a4ffd0aa8ebba87f0bb60e9885094d0af4c35ffe
Sebazzz <triadsebas@gmail.com>
2017-04-30 22:40:36 +0200

    Enable auto completion for contact information

--- src/IFS.Web/wwwroot/js/site.js
+++ src/IFS.Web/wwwroot/js/site.js
@@ -1,1 +1,30 @@
-﻿// Write your Javascript code.
+﻿(function(app, storage) {
+    app.contactInformationPersistence = {
+        initialize: function(nameFieldId, emailFieldId) {
+            function initFromLocalStorage(key, id) {
+                if (!storage) {
+                    return;
+                }
+
+                var value = storage.getItem(key),
+                    element = document.getElementById(id);
+
+                if (!element) {
+                    return;
+                }
+
+                if (value) {
+                    element.value = value;
+                }
+
+                element.addEventListener('change', function() {
+                    value = element.value;
+                    storage.setItem(key, value);
+                });
+            }
+
+            initFromLocalStorage('name', nameFieldId);
+            initFromLocalStorage('type', emailFieldId);
+        }
+    }
+})(window.app = (window.app || {}), localStorage);
