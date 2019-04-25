646e9c694bba2940396adeddf82a25289621068c
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-04-25 18:34:04 +0200

    Fix endpoint routing breaking URL generation by disabling it for now

--- src/IFS.Web/Startup.cs
+++ src/IFS.Web/Startup.cs
@@ -8,7 +8,8 @@
 using IFS.Web.Framework.Middleware.Fail2Ban;
 using IFS.Web.Framework.Services;
 
-namespace IFS.Web {
+namespace IFS.Web
+{
     using System;
 
     using Core;
@@ -28,7 +29,8 @@ namespace IFS.Web {
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Options;
 
-    public sealed class Startup {
+    public sealed class Startup
+    {
         public Startup(IConfiguration env)
         {
             this.Configuration = env;
@@ -37,9 +39,11 @@ namespace IFS.Web {
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             // Add framework services.
-            services.AddRouting(routing => {
+            services.AddRouting(routing =>
+            {
                 routing.LowercaseUrls = true;
 
             });
@@ -48,9 +52,14 @@ namespace IFS.Web {
 
             services.AddMvc(mvc => mvc.ModelBinderProviders.Insert(0, new FileIdentifierModelBinderProvider()))
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddMvcOptions(opts => opts.InputFormatterExceptionPolicy = InputFormatterExceptionPolicy.AllExceptions);
+                .AddMvcOptions(opts =>
+                {
+                    opts.InputFormatterExceptionPolicy = InputFormatterExceptionPolicy.AllExceptions;
+                    opts.EnableEndpointRouting = false;
+                });
 
-            services.AddAuthorization(opt => {
+            services.AddAuthorization(opt =>
+            {
                 opt.AddUploadPolicy(this.Configuration);
                 opt.AddAdministrationPolicy(this.Configuration);
             });
@@ -59,7 +68,8 @@ namespace IFS.Web {
                 .AddFromSettings(this.Configuration);
 
             // Hangfire
-            services.AddHangfire(config => {
+            services.AddHangfire(config =>
+            {
                 config.UseColouredConsoleLogProvider();
                 config.UseMemoryStorage();
             });
@@ -96,27 +106,32 @@ namespace IFS.Web {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
             app.UseStatusCodePagesWithReExecute("/Error/Http-{0}");
 
-            if (env.IsDevelopment()) {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
-            } else {
+            }
+            else
+            {
                 app.UseExceptionHandler("/Error/Error");
 
                 // Come on, it is >= 2019, so anything stuff should be done over HTTPS
                 app.UseHttpsRedirection();
                 app.UseHsts();
             }
-            
+
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseFail2BanRecording();
 
             // Hangfire
             app.UseHangfireDashboard(
-                pathMatch: "/administration/jobs", 
-                options: new DashboardOptions {
+                pathMatch: "/administration/jobs",
+                options: new DashboardOptions
+                {
                     AppPath = "/administration/",
                     Authorization = new[] {
                         new AdministratorDashboardFilter(app.ApplicationServices)
@@ -127,7 +142,8 @@ namespace IFS.Web {
 
             // MVC and API
             app.UseMvc(
-                routes => {
+                routes =>
+                {
                     // Async uploads - need to map this here as MVC won't generate routes to other IRouter
                     routes.MapUploadHandler("upload/handler/{fileIdentifier}");
 
@@ -147,23 +163,28 @@ namespace IFS.Web {
                 "*/30 * * * *");
         }
 
-        private sealed class AdministratorDashboardFilter : IDashboardAuthorizationFilter {
+        private sealed class AdministratorDashboardFilter : IDashboardAuthorizationFilter
+        {
             private readonly IServiceProvider _serviceProvider;
 
-            public AdministratorDashboardFilter(IServiceProvider serviceProvider) {
+            public AdministratorDashboardFilter(IServiceProvider serviceProvider)
+            {
                 this._serviceProvider = serviceProvider;
             }
 
-            public bool Authorize(DashboardContext context) {
+            public bool Authorize(DashboardContext context)
+            {
                 var auth = this._serviceProvider.GetRequiredService<IOptions<Core.Authentication.AuthenticationOptions>>().Value?.Static.Administration;
 
-                if (auth == null) {
+                if (auth == null)
+                {
                     return false;
                 }
 
                 var ctx = (context as AspNetCoreDashboardContext)?.HttpContext;
 
-                if (ctx == null) {
+                if (ctx == null)
+                {
                     return false;
                 }
 
