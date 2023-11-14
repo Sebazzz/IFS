2bc4adc24cc4ceb6624c74925ecb8a7b58378684
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2023-11-14 20:34:18 +0100

    Switch to generic HostBuilder

--- src/IFS.Web/Program.cs
+++ src/IFS.Web/Program.cs
@@ -5,35 +5,33 @@
 //  Project         : IFS.Web
 // ******************************************************************************
 
-using Microsoft.Extensions.Hosting;
 using System.IO;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace IFS.Web;
 
-public sealed class Program {
-    public static void Main(string[] args) {
-        IWebHost host = 
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.local.json", true))
-                .CaptureStartupErrors(true)
-                .ConfigureLogging((wc,logging) => {
-                    var env = wc.HostingEnvironment;
-                    var config = wc.Configuration;
+public sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(wc => wc.UseStartup<Startup>().CaptureStartupErrors(true))
+            .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.local.json", true))
+            .ConfigureLogging((wc, logging) =>
+            {
+                var env = wc.HostingEnvironment;
+                var config = wc.Configuration;
 
-                    logging.AddConfiguration(config.GetSection("Logging"));
-                    logging.AddConsole();
+                logging.AddConfiguration(config.GetSection("Logging"));
+                logging.AddConsole();
 
-                    if (env.IsDevelopment()) {
-                        logging.AddDebug();
-                    }
-                })
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                .Build();
+                if (env.IsDevelopment()) logging.AddDebug();
+            })
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .Build();
 
         host.Run();
     }
