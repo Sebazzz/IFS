4cd9362dc081640fbbaacc70c13981cb1b9bba27
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2024-04-09 22:13:12 +0200

    Refactor Program.cs - implicit class

--- src/IFS.Web/Program.cs
+++ src/IFS.Web/Program.cs
@@ -6,32 +6,24 @@
 // ******************************************************************************
 
 using System.IO;
+using IFS.Web;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 
-namespace IFS.Web;
+var host = Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(wc => wc.UseStartup<Startup>().CaptureStartupErrors(true))
+    .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.local.json", true))
+    .UseSerilog((context, svc, config) =>
+        config.ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(svc)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Application", "IFS")
+            .WriteTo.Console()
+            .WriteTo.Debug()
+    )
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .Build();
 
-public sealed class Program
-{
-    public static void Main(string[] args)
-    {
-        var host = Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(wc => wc.UseStartup<Startup>().CaptureStartupErrors(true))
-            .ConfigureAppConfiguration(x => x.AddJsonFile("appsettings.local.json", true))
-            .UseSerilog((context, svc, config) => 
-                config.ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(svc)
-                    .Enrich.FromLogContext()
-                    .Enrich.WithProperty("Application", "IFS")
-                    .WriteTo.Console()
-                    .WriteTo.Debug()
-            )
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .Build();
-
-        host.Run();
-    }
-}
+host.Run();
